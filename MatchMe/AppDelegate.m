cfeb7a82398722d278ddfa5047f00d7982d937b9
Yi Wang <yiwang1982@Yis-MacBook-Air.local>
2014-08-25 22:20:31 -0400

    Setup Facebook and Parse

--- MatchMe/AppDelegate.m
+++ MatchMe/AppDelegate.m
@@ -16,14 +16,21 @@
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
-    
+    // setup Parse
     [Parse setApplicationId:@"dZRAQckigLd2pJFIKZoj84bsH3AHK1M2hglsaM3Q"
                   clientKey:@"QGFLiMRRDEBuRG24L84nm5M9iDkaMm3qgnApGXjh"];
+    [PFFacebookUtils initializeFacebook];
+    
+    
+    
     
     return YES;
 }
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
